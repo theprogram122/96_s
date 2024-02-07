d9deb76a82dfc0a6a174aa4d8b6b208a52b1efd4
theprogram122 <157767328+theprogram122@users.noreply.github.com>
2024-02-06 19:37:24 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    player1_name.localStorage.setItem()
+    player1_name.locolstorage.setItem()
+    window.location = "game_page.html";
+}
+
